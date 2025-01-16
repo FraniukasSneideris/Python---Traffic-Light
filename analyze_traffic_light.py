617523aa5d177fe8446787f303005b5b115aac42
FraniukasSneideris <herrerafrane@gmail.com>
2025-01-16 21:44:46 +0200

    Update and rename analyze_traffic_light_analyzer.py to analyze_traffic_light.py

--- analyze_traffic_light.py
+++ analyze_traffic_light.py
@@ -1,4 +1,4 @@
-# analyze_traffic_light_analyzer.py
+# analyze_traffic_light.py
 
 # Creating a rule-based dictionary for valid transitions, knowing that the only allowed sequences are:
 # red, yellow, green, yellow, red
@@ -12,20 +12,18 @@ rule_set = {
     (0, 0, 0, 0): [(0, 1, 0, 0), (0, 0, 1, 0), (0, 0, 0, 1), (0, 0, 0, 0)]  # no light on -> yellow, green, green left, or no light on
 }
 
-def main(file_path):
-    data = open_file(file_path)
-    traffic_light_data = process_data(data)
+def main(file):
+    data = open_traffic_light_file(file)
+    traffic_light_data = process_traffic_light_data(data)
     analyze_traffic_light(traffic_light_data)
 
 # Opening the file
-def open_file(file):
+def open_traffic_light_file(file):
   with open(file, 'r') as f:
     return f.readlines()
 
-data = open_file(file_path)
-
 # Processing the data: removing '\n', splitting with commas and converting to int
-def process_data(data):
+def process_traffic_light_data(data):
   processed_data = []
   for line in data:
     stripped_line = line.strip()
@@ -34,8 +32,6 @@ def process_data(data):
     processed_data.append(integer_line)
   return processed_data
 
-traffic_light_data = process_data(data)
-
 # Testing the traffic light
 def analyze_traffic_light(traffic_light_data):
     for i in range(0, len(traffic_light_data) - 1):
@@ -43,12 +39,9 @@ def analyze_traffic_light(traffic_light_data):
         next_state = tuple(traffic_light_data[i + 1])
 
         assert next_state in rule_set.get(current_state, []), \
-            f"Traffic light does not work. Invalid transition detected at index {i}: transition from {current_state} to {next_state}" 
+            f"Traffic light does not work. Invalid transition detected at index {i}: transition from {current_state} to {next_state}"
 
     print("Traffic light works!")
 
-# Calling the final function to test the traffic light:
-analyze_traffic_light(traffic_light_data)
-
 if __name__ == "__main__":
-    main("traffic_light_data.txt")
+    main('traffic_light_data_file.txt')
